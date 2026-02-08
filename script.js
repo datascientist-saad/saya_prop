a8998dc00db8fd3c59b89ef90bf688333f3e9b1d
Muhammad Saad <54408261+datascientist-saad@users.noreply.github.com>
2026-02-08 09:36:51 +0300

    Upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function flipCard(){
+document.getElementById('cardInner').style.transform='rotateY(180deg)';
+}
+function yes(){
+document.getElementById('message').innerHTML='💞 I love you endlessly 💞<br>You just made me the happiest person alive 🥹❤️';
+}
